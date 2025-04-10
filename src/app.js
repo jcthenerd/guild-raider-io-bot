27c6054227db3d7c541091e42554fda529aca027
Blachloch <jcollins.95@live.com>
2025-04-10 02:48:54 -0400

    Remove express listener for netlify listener

--- src/app.js
+++ src/app.js
@@ -84,8 +84,4 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
   return res.status(400).json({ error: 'unknown interaction type' });
 });
 
-app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
-});
-
 export const handler = serverless(app);
